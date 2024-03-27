57e7ff2834c13c3043bf8d55c74036e21dec0200
pnaruka <pankajnaruka111@gmail.com>
2024-03-27 15:35:09 +0530

    login and signup init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route path="/" element={<HomePage/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
       <Route path="/chats" element={<ChatPage/>} />
       </Routes>
     </div>
