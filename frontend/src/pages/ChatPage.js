62d8090b4cbea34f908b1768707f0141bafde8bc
pnaruka <pankajnaruka111@gmail.com>
2024-03-27 12:00:19 +0530

    chat page api test

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
 
 const ChatPage = () => {
+
+  const fetchChat = async()=>{
+    const data = await axios.get('/');
+    console.log(data);
+  }
+  useEffect(()=>{
+    fetchChat();
+  },[])
   return (
     <div>ChatPage</div>
   )
