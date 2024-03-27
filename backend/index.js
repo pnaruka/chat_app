ae85a453752d73dadb1a0ba53ab1841ee3c748ac
pnaruka <pankajnaruka111@gmail.com>
2024-03-27 10:27:45 +0530

    initial frontend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+app.get('/', (req,res)=>{
+    return res.send("Hallo");
+})
+
 app.listen(port, ()=>{
     console.log('Server up');
 });
