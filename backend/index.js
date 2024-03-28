8cb5181fb0184e16396b1518c23928965e1c3cff
pnaruka <pankajnaruka111@gmail.com>
2024-03-28 13:47:09 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
+const { configDotenv } = require('dotenv');
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
+configDotenv({path:'./.env'});
+const DB_URL = process.env.DB_URL;
 
 app.get('/', (req,res)=>{
     return res.send("Hallo");
 })
 
-app.listen(port, ()=>{
-    console.log('Server up');
-});
+mongoose.connect(DB_URL).then(()=>{
+    console.log('DB Connected');
+    app.listen(port, ()=>{
+        console.log('Server up');
+    });
+}).catch((error)=>{
+    console.log(error);
+})
