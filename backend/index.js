80b8f496d683dc2a32069e97e9955fea2b70dc9c
pnaruka <pankajnaruka111@gmail.com>
2024-03-31 10:02:10 +0530

    use cors

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const UserRouter = require('./routes/UserRouter');
 const ChatRouter = require('./routes/ChatRouter');
 const requireAuth = require('./middlewares/requireAuth');
 const { notFound, errorHandler } = require('./middlewares/errorHandlers');
+const cors = require('cors');
 
 configDotenv({path:'./.env'});
 const port = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use('/user',UserRouter);
 app.use(requireAuth);
 app.use('/chat', ChatRouter);
+app.use(cors());
 
 app.get('/', (req,res)=>{
     return res.send("Hallo");
@@ -27,7 +29,7 @@ app.use(errorHandler);
 mongoose.connect(DB_URL).then(()=>{
     console.log('DB Connected');
     app.listen(port, ()=>{
-        console.log('Server up');
+        console.log(`Server up at ${port}`);
     });
 }).catch((error)=>{
     console.log(error);
