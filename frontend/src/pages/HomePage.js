895aa479cc66dad463e649c94be836bda2000fb5
pnaruka <pankajnaruka111@gmail.com>
2024-04-01 10:19:31 +0530

    message page added

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,27 +1,31 @@
 import React from 'react'
-import { assignUSer, getUser } from '../contexts_store/reducer/user'
-import { assignChats, getChats } from '../contexts_store/reducer/chats'
-import { assignMsg, getMsg } from '../contexts_store/reducer/messages'
-import { useDispatch, useSelector } from 'react-redux'
+//import { assignUSer, getUser } from '../contexts_store/reducer/user'
+//import { assignChats, getChats } from '../contexts_store/reducer/chats'
+//import { assignMsg, getMsg } from '../contexts_store/reducer/messages'
+//import { useDispatch, useSelector } from 'react-redux'
+import ChatPage from './ChatPage';
+import Messages from './Messages';
+import './Homepage.css'
 
 const HomePage = () => {
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
 
-  const handleClick = async()=>{
-    /*
-    dispatch(assignUSer({name: "Pran B", email: "pranb@gmail.com"}));
-    dispatch(assignChats([{a:"1"},{a:"2"},{a:"3"}]));
-    dispatch(assignMsg([{b:"1"},{b:"2"},{b:"3"}]));
-*/
+  const handleClick = async () => {
     console.log('Click');
-    //console.log(chats);
-    //console.log(msg);
   }
 
   return (
     <div>
       <h1>HomePage</h1>
       <button onClick={handleClick}>Click</button>
+      <div className='container'>
+        <div className='chat-page'>
+          <ChatPage />
+        </div>
+        <div className='msg-page'>
+          <Messages />
+        </div>
+      </div>
     </div>
   )
 }
