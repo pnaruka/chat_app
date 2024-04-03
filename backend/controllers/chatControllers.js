1602d6c928d0aaedc153bd1395968f4be0d8f17c
pnaruka <pankajnaruka111@gmail.com>
2024-04-03 10:26:43 +0530

    create and delete group route added

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -44,7 +44,7 @@ const createChat = asyncHandler(async (req, res) => {
 
 const getAllChats = asyncHandler(async (req, res) => {
     const userId = req.user._id;
-
+    //console.log(req.user);
     try {
         const chat = await ChatModel.find({
             $and: [
@@ -52,7 +52,7 @@ const getAllChats = asyncHandler(async (req, res) => {
             ]
         }).populate("users", "-password")
             .populate("lastMessage")
-            .populate("groupAdmin")
+            .populate("groupAdmin", "-password")
             .sort({ updatedAt: -1 });
 
         if (!chat) {
@@ -66,7 +66,70 @@ const getAllChats = asyncHandler(async (req, res) => {
 
 })
 
+const createGroup = asyncHandler(async (req, res) => {
+    if (!req.body.users || !req.body.groupName) {
+        return res.status(400).json("Please fill all the fields.");
+    }
+
+    var users = JSON.parse(req.body.users);
+
+    if (users.length < 2) {
+        return res.status(400).json("Please add at least 2 members.");
+    }
+
+    try {
+        users.push(req.user._id);
+        const groupChat = await ChatModel.create({
+            chatName: req.body.chatName,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.user._id //check please
+        });
+        const fullGroupChat = await ChatModel.find({
+            _id: groupChat._id
+        }).populate("users", "-password")
+            .populate("lastMessage")
+            .populate("groupAdmin", "-password")
+            .sort({ updatedAt: -1 });
+
+        res.status(201).json(fullGroupChat);
+
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+
+});
+
+const deleteGroup = asyncHandler( async(req, res)=>{
+    const {groupId} = req.body;
+    if(!groupId){
+        return res.status(400).send("Please provide the group ID");
+    }
+
+    const fullChat = await ChatModel.findOne({
+        _id: groupId
+    }).populate("groupAdmin", "-password");
+    //console.log(fullChat);
+    if(!fullChat || fullChat.length === 0){
+        return res.status(400).send("No such group exists");
+    }
+    //console.log( typeof fullChat.groupAdmin._id);
+    //console.log(typeof req.user._id);
+    if(!fullChat.groupAdmin._id.equals(req.user._id)){
+        return res.status(401).send("Only the admin can delete the group.");
+    }
+
+    try {
+        await ChatModel.findByIdAndDelete({ _id: groupId });
+        return res.status(200).send("Group deleted successfully.");
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
 module.exports = {
     createChat,
-    getAllChats
+    getAllChats,
+    createGroup,
+    deleteGroup
 }
--- backend/routes/ChatRouter.js
+++ backend/routes/ChatRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createChat, getAllChats } = require('../controllers/chatControllers');
+const { createChat, getAllChats, createGroup, deleteGroup } = require('../controllers/chatControllers');
 
 const ChatRouter = express.Router();
 
 ChatRouter.post('/create', createChat);
 ChatRouter.get('/show', getAllChats);
+ChatRouter.post('/group/create', createGroup);
+ChatRouter.delete('/group/delete', deleteGroup);
 
 module.exports = ChatRouter;
