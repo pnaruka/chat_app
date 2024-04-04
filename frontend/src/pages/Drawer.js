abf7ea35534b7e45695efa8e79eecf90336e74a4
pnaruka <pankajnaruka111@gmail.com>
2024-04-04 11:55:22 +0530

    navbar and side drawer

---
+++ frontend/src/pages/Drawer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Drawer = () => {
+    return (
+        <nav className="navbar bg-body-tertiary fixed-top">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">baat</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Find someone to talk to</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <form className="d-flex mt-3" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Type a name" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Drawer
