9948abb8248c66c42f03c8781ccf50d125d71b2f
pnaruka <pankajnaruka111@gmail.com>
2024-04-05 10:56:19 +0530

    use context for fetching chats

---
+++ frontend/src/hooks/useFetchChats.js
@@ -0,0 +1,44 @@
+import { useState} from "react";
+import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { assignChats } from "../contexts_store/reducer/chats";
+
+
+export const useFetchChats = ()=>{
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const dispatch = useDispatch();
+
+    const fetchChats = async (user)=>{
+        setIsLoading(true);
+        setError(null);
+        const headers = { 'Authorization': `Bearer ${user.token}` };
+        await axios.get('http://localhost:5000/chat/show', {
+            headers: headers
+          })
+        .then((res)=>{
+            setIsLoading(false);
+            dispatch(assignChats(res.data.map((chat) => {
+                if (chat.isGroupChat)
+                  return {
+                    _id: chat._id,
+                    chatName: chat.chatName,
+                    users: chat.users
+                  }
+                else
+                  return {
+                    _id: chat._id,
+                    chatName: chat.users.find(u => u.email !== user.email).name,
+                    users: chat.users
+                  }
+              })))
+        })
+        .catch((error)=>{
+            setIsLoading(false);
+            setError(error.response.data.message);
+        });
+
+        setIsLoading(false);
+    }
+    return {fetchChats, isLoading, error};
+};
--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,53 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { getUser } from '../contexts_store/reducer/user';
+import { useFetchChats } from '../hooks/useFetchChats';
+import { getChats } from '../contexts_store/reducer/chats';
 
 const ChatPage = () => {
   const user = useSelector(getUser);
-  const [chats, setChats] = useState(null);
-
-  const fetchChat = async () => {
-    const headers = { 'Authorization': `Bearer ${user.token}` };
-    //const data = 
-    await axios.get('http://localhost:5000/chat/show', {
-      headers: headers
-    })
-      .then((res) => {
-        console.log(res);
-        setChats(res.data.map((chat) => {
-          if (chat.isGroupChat)
-            return {
-              _id: chat._id,
-              chatName: chat.chatName,
-              users: chat.users
-            }
-          else
-            return {
-              _id: chat._id,
-              chatName: chat.users.find(u => u.email !== user.email).name,
-              users: chat.users
-            }
-        }))
-      })
-      .catch((error) => {
-        //console.log(error);
-      });
-      
-    console.log(chats);
-  };
+  const chats = useSelector(getChats);
+  const {fetchChats, error} = useFetchChats();
 
   useEffect(() => {
-    if (user)
-      fetchChat();
+    //if (user)
+    console.log(user);
+    fetchChats(user);
   }, [user]);
 
   return (
     <div>
       <h6>Chats</h6>
+      {
+        error ?
+        <div>
+          <p>No chats to show</p>
+        </div>
+        : <></>
+      }
       {chats ?
         <ul className='list-group list-group-flush list-group-item-light'>
-          {chats.map((chat) => <li className='list-group-item' key={chat._id}>{chat.chatName}</li>)}
+          {chats.map((chat) => <li className='list-group-item' key={chat._id}>
+            <div className="row row-cols-1 row-cols-md-1 g-4">
+              <div className="col">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{chat.chatName}</h5>
+                    <p className="card-text">{chat.lastMessage || "No message"}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>)}
         </ul>
         :
         <></>
